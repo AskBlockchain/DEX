fbb4fd891c2efbb827493dc777cc22768fcfcd6e
AskBlockchain <120322775+AskBlockchain@users.noreply.github.com>
2022-12-30 14:48:55 +0000

    Build 1.4

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -8,7 +8,7 @@ import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "../node_modules/@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
-contract wallet is Ownable{
+contract Wallet is Ownable{
 
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,6 +1,13 @@
 const Link = artifacts.require("Link");
+const Wallet = artifacts.require("Wallet");
 
-module.exports = function (deployer) {
+
+module.exports = async function (deployer) {
   deployer.deploy(Link);
-};
+  let wallet = await Wallet.deployed();
+  let link = await Link.deployed();
+  await link.approve(wallet.address, 300);
+  await wallet.addToken(web3.utils.fromUtf8("LINK"), link.address);
+  await wallet.deposit(100, web3.utils.fromUtf8("LINK"));  
+ };
 
